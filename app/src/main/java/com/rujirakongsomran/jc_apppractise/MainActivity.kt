86401c45ff30ea9ee208d00205c45dfdec8f63ff
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-10-20 13:54:40 +0700

    Create small top app bar

--- app/src/main/java/com/rujirakongsomran/jc_apppractise/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_apppractise/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,25 +26,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SmallTopAppBarExample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ), title = {
+                    Text(text = "Small Top App Bar")
+                }
+            )
+        }
+    ) { innerPadding ->
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JC_AppPractiseTheme {
-        Greeting("Android")
     }
 }
+
